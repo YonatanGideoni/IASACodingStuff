8cae626119227d7f60c16a84160df9a96623f35d
YonatanGideoni <yonatan.gideoni@gmail.com>
2016-09-10 14:25:31 +0300

    Spiral drill

---
+++ BadSpiralNum/BadSpiralNum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadSpiralNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5, 8];
+            int up = 0, down = a.GetLength(0) - 1, left = 0, right = a.GetLength(1) - 1;
+
+            for (int i = 0; i < a.GetLength(0) * a.GetLength(1); ) { 
+                for (int k = left; k < right + 1 && i < a.GetLength(0) * a.GetLength(1); k++)
+                {
+                    a[up, k] = i;
+                    i++;
+                    Console.SetCursorPosition(3 * k, 3 * up);
+                    Console.Write("{0}", a[up, k]);
+                }
+                up++;
+
+                for (int k = up; k < down + 1 && i < a.GetLength(0) * a.GetLength(1); k++)
+                {
+                    a[k, right] = i;
+                    i++;
+                    Console.SetCursorPosition(3 * right, 3 * k);
+                    Console.Write("{0}", a[k, right]);
+                }
+                right--;
+
+                for (int k = right; k > left - 1 && i < a.GetLength(0) * a.GetLength(1); k--)
+                {
+                    a[down,k] = i;
+                    i++;
+                    Console.SetCursorPosition(3 * k, 3 * down);
+                    Console.Write("{0}", a[down,k]);
+                }
+                down--;
+
+                for (int k = down; k > up - 1 && i < a.GetLength(0) * a.GetLength(1); k--)
+                {
+                    a[k, left] = i;
+                    i++;
+                    Console.SetCursorPosition(3 * left, 3 * k);
+                    Console.Write("{0}", a[k, left]);
+                }
+                left++;
+            }
+        }
+    }
+}
---
+++ SpiralNum/SpiralNum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SpiralNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int totalWidth = 7;
+            int totalHeight = 10;
+            int[,] a = new int[totalWidth, totalHeight];
+            int[] dir = new int[4] {1, 0, -1, 0 };
+            int x = 0;
+            int y = 0;
+            int XCorner = 0;
+            int YCorner = 0;
+            int CornerSum = 0;
+
+            for (int i = 0; i < totalWidth * totalHeight; i++ )
+            {
+                XCorner = (i + totalHeight - XCorner) / (totalWidth + totalHeight - 1 - YCorner);
+                YCorner = (i - YCorner) / (totalWidth + totalHeight - 1 - XCorner);
+                CornerSum = XCorner + YCorner;
+                int XdirNum = (CornerSum) % dir.Length;
+                int YdirNum = (dir.Length - 1 + CornerSum) % dir.Length;
+                a[x, y] = i;
+                Console.SetCursorPosition(3 * x, 3 * y);
+                Console.Write("{0}", a[x, y]);
+                x += dir[XdirNum];
+                y += dir[YdirNum];
+            }
+            Console.SetCursorPosition(3 * totalWidth, 3 * totalHeight);
+            Thread.Sleep(99999999);
+        }
+    }
+}
