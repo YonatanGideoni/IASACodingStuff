acae0d359b8009590ae924268777a78d25db185d
YonatanGideoni <yonatan.gideoni@gmail.com>
2016-10-01 15:48:04 +0300

    Reversi WIP
    WIP stages of Reversi project.

---
+++ Reversi/Reversi/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Reversi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GridSizeBox = new System.Windows.Forms.NumericUpDown();
+            this.GameTimeBox = new System.Windows.Forms.TextBox();
+            this.BoardPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.GridSizeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(13, 13);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(75, 23);
+            this.RestartButton.TabIndex = 0;
+            this.RestartButton.Text = "New Game";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(127, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(43, 13);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Grid Size:";
+            // 
+            // GridSizeBox
+            // 
+            this.GridSizeBox.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.GridSizeBox.Location = new System.Drawing.Point(176, 13);
+            this.GridSizeBox.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.GridSizeBox.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.GridSizeBox.Name = "GridSizeBox";
+            this.GridSizeBox.Size = new System.Drawing.Size(32, 20);
+            this.GridSizeBox.TabIndex = 2;
+            this.GridSizeBox.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // GameTimeBox
+            // 
+            this.GameTimeBox.BackColor = System.Drawing.SystemColors.Control;
+            this.GameTimeBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.GameTimeBox.Location = new System.Drawing.Point(247, 12);
+            this.GameTimeBox.Name = "GameTimeBox";
+            this.GameTimeBox.ReadOnly = true;
+            this.GameTimeBox.Size = new System.Drawing.Size(46, 13);
+            this.GameTimeBox.TabIndex = 3;
+            this.GameTimeBox.Text = "Time: 0";
+            // 
+            // BoardPanel
+            // 
+            this.BoardPanel.Location = new System.Drawing.Point(13, 62);
+            this.BoardPanel.Name = "BoardPanel";
+            this.BoardPanel.Size = new System.Drawing.Size(35, 36);
+            this.BoardPanel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 262);
+            this.Controls.Add(this.BoardPanel);
+            this.Controls.Add(this.GameTimeBox);
+            this.Controls.Add(this.GridSizeBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RestartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GridSizeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown GridSizeBox;
+        private System.Windows.Forms.TextBox GameTimeBox;
+        private System.Windows.Forms.Panel BoardPanel;
+    }
+}
+
---
+++ Reversi/Reversi/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+    public partial class Form1 : Form
+    {
+        Button[,] Board;
+        short[,] intBrd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            short GridLength = Convert.ToInt16(GridSizeBox.Value + 2);
+            short ButtonSize = 20;
+
+            Board = new Button[GridLength, GridLength];
+            intBrd = new short[GridLength, GridLength];
+
+            BoardPanel.Size = new Size(GridLength * ButtonSize, GridLength * ButtonSize);
+            this.Size = new Size(400, BoardPanel.Height + BoardPanel.Location.Y + 50);
+
+            for (int i = 0; i < GridLength; i++)
+            {
+                for (int k = 0; k < GridLength; k++)
+                {
+                    Board[i, k] = new Button();
+                    Board[i, k].Size = new Size(ButtonSize, ButtonSize);
+                    Board[i, k].Location = new Point(ButtonSize * i, ButtonSize * k);
+                    intBrd[i, k] = 0;
+                    Board[i, k].BackColor = default(Color);
+                    Board[i, k].Tag = i.ToString() + k.ToString();
+
+                    BoardPanel.Controls.Add(Board[i, k]);
+                    Board[i, k].Click += Form1_Click;
+
+                    if (i == 0 || i == GridLength - 1 || k == 0 || k == GridLength - 1)
+                    {
+                        Board[i, k].Enabled = false;
+                        Board[i, k].Visible = false;
+                    }
+                }
+            }
+
+            Board[GridLength / 2, GridLength / 2].BackColor = Color.Black;
+            Board[GridLength / 2 - 1, GridLength / 2].BackColor = Color.White;
+            Board[GridLength / 2, GridLength / 2 - 1].BackColor = Color.White;
+            Board[GridLength / 2 - 1, GridLength / 2 - 1].BackColor = Color.Black;
+
+            intBrd[GridLength / 2, GridLength / 2] = 1;
+            intBrd[GridLength / 2 - 1, GridLength / 2] = 2;
+            intBrd[GridLength / 2, GridLength / 2 - 1] = 2;
+            intBrd[GridLength / 2 - 1, GridLength / 2 - 1] = 1;
+        }
+
+        public bool CanPlace(short type, short row, short col)
+        {
+
+            for (short i = 1; i < Board.GetLength(0) - row - 2; i++)
+            {
+                if (type != intBrd[row + i, col] && intBrd[row + i, col] != 0)//check if a touching tile is an enemy tile
+                {
+                    continue;
+                }
+                else if (intBrd[row + i, col] == 0) //if it is an uninhabited tile, stop checking
+                {
+                    break;
+                }
+                else //if the next tile over is a friendly tile, a friendly tile may be placed  
+                {
+                    return true;
+                }
+            }
+            // Same deal for all the other loops. Each checks a different direction
+            for (short i = 0; i < Board.GetLength(0) - col - 2; i++)
+            {
+                if (type != intBrd[row, col + i] && intBrd[row, col + i] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row, col + i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            for (short i = 0; i < col; i++)
+            {
+                if (type != intBrd[row, col - i] && intBrd[row, col - i] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row, col - i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            for (short i = 0; i < row; i++)
+            {
+                if (type != intBrd[row - i, col] && intBrd[row - i, col] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row - i, col] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            for (short i = 0; i < row && i < col; i++)
+            {
+                if (type != intBrd[row - i, col - i] && intBrd[row - i, col - i] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row - i, col - i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            for (short i = 0; i < Board.GetLength(0) - 2 - row && i < col; i++)
+            {
+                if (type != intBrd[row + i, col - i] && intBrd[row + i, col - i] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row + i, col - i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            for (short i = 0; i < Board.GetLength(0) - 2 - row && i < Board.GetLength(0) - 2 - col; i++)
+            {
+                if (type != intBrd[row + i, col + i] && intBrd[row + i, col + i] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row + i, col + i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            for (short i = 0; i < row && i < Board.GetLength(0) - 2 - col; i++)
+            {
+                if (type != intBrd[row - i, col + i] && intBrd[row - i, col + i] != 0)
+                {
+                    continue;
+                }
+                else if (intBrd[row - i, col + i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            short ButtonLoc = short.Parse(((Button)(sender)).Tag.ToString());
+            short GridLength = Convert.ToInt16(GridSizeBox.Value);
+            short row = (short)(ButtonLoc / GridLength);
+            short col = (short)(ButtonLoc % GridLength);
+
+            if (CanPlace(1, row, col) == true)
+            {
+                MessageBox.Show("Can Place!");
+            }
+            else
+            {
+                MessageBox.Show("Can't Place Here :(");
+            }
+        }
+    }
+}
