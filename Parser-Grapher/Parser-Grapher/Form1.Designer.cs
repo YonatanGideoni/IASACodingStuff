297969e47e13df7a9b20814325e6b92fad5247e6
YonatanGideoni <yonatan.gideoni@gmail.com>
2017-05-02 10:59:53 +0300

    Start of Grapher Project
    Trying to create a parser and afterwards a grapher as a bonus project.
    Should be a hefty ordeal.

---
+++ Parser-Grapher/Parser-Grapher/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Parser_Grapher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FunctionText = new System.Windows.Forms.TextBox();
+            this.ParseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FunctionText
+            // 
+            this.FunctionText.Location = new System.Drawing.Point(76, 57);
+            this.FunctionText.Name = "FunctionText";
+            this.FunctionText.Size = new System.Drawing.Size(100, 20);
+            this.FunctionText.TabIndex = 0;
+            this.FunctionText.TextChanged += new System.EventHandler(this.FunctionText_TextChanged);
+            // 
+            // ParseButton
+            // 
+            this.ParseButton.Location = new System.Drawing.Point(76, 13);
+            this.ParseButton.Name = "ParseButton";
+            this.ParseButton.Size = new System.Drawing.Size(75, 23);
+            this.ParseButton.TabIndex = 1;
+            this.ParseButton.Text = "Parse";
+            this.ParseButton.UseVisualStyleBackColor = true;
+            this.ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ParseButton);
+            this.Controls.Add(this.FunctionText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FunctionText;
+        private System.Windows.Forms.Button ParseButton;
+    }
+}
+
---
+++ Parser-Grapher/Parser-Grapher/ParseTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser_Grapher
+{
+    public class ParseTree<T>
+    {
+        private ParseTree<T> left;
+        private T operation;
+        private ParseTree<T> right;
+
+        public ParseTree(T operand)
+        {
+            this.left = null;
+            this.right = null;
+            this.operation = operand;
+        }
+
+        public T getOperand()
+        {
+            return this.operation;
+        }
+
+        public void setOperand(T operand)
+        {
+            this.operation = operand;
+        }
+
+        public void setRight(ParseTree<T> newRight)
+        {
+            this.right = newRight;
+        }
+
+        public void setLeft(ParseTree<T> newLeft)
+        {
+            this.left = newLeft;
+        }
+    }
+    }
+}
