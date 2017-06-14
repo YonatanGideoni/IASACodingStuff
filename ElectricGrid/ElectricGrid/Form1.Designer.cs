99877920e0c0dc6d62890d315591b6d052874a34
YonatanGideoni <yonatan.gideoni@gmail.com>
2017-06-14 19:19:32 +0300

    Start of Electric Grid
    Start of creating an electric grid to train with lists. Need to create the class.

---
+++ ElectricGrid/ElectricGrid/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ElectricGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InitGridButton = new System.Windows.Forms.Button();
+            this.GridSizeNum = new System.Windows.Forms.NumericUpDown();
+            this.GridSizeText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GridSizeNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InitGridButton
+            // 
+            this.InitGridButton.Location = new System.Drawing.Point(13, 13);
+            this.InitGridButton.Name = "InitGridButton";
+            this.InitGridButton.Size = new System.Drawing.Size(75, 23);
+            this.InitGridButton.TabIndex = 0;
+            this.InitGridButton.Text = "Create Grid";
+            this.InitGridButton.UseVisualStyleBackColor = true;
+            this.InitGridButton.Click += new System.EventHandler(this.InitGridButton_Click);
+            // 
+            // GridSizeNum
+            // 
+            this.GridSizeNum.Location = new System.Drawing.Point(186, 13);
+            this.GridSizeNum.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.GridSizeNum.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.GridSizeNum.Name = "GridSizeNum";
+            this.GridSizeNum.Size = new System.Drawing.Size(37, 20);
+            this.GridSizeNum.TabIndex = 1;
+            this.GridSizeNum.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // GridSizeText
+            // 
+            this.GridSizeText.BackColor = System.Drawing.SystemColors.Control;
+            this.GridSizeText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.GridSizeText.Location = new System.Drawing.Point(131, 15);
+            this.GridSizeText.Name = "GridSizeText";
+            this.GridSizeText.Size = new System.Drawing.Size(49, 13);
+            this.GridSizeText.TabIndex = 2;
+            this.GridSizeText.Text = "Grid Size:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.GridSizeText);
+            this.Controls.Add(this.GridSizeNum);
+            this.Controls.Add(this.InitGridButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GridSizeNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InitGridButton;
+        private System.Windows.Forms.NumericUpDown GridSizeNum;
+        private System.Windows.Forms.TextBox GridSizeText;
+    }
+}
+
---
+++ ElectricGrid/ElectricGrid/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectricGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] gridArr;
+        byte[,] gridByte;
+        byte[] prevButton;
+
+        private void InitGridButton_Click(object sender, EventArgs e)
+        {
+            byte gridSize = (byte)GridSizeNum.Value;
+            byte buttonSize = 20;
+            gridArr = new Button[gridSize, gridSize];
+            gridByte = new byte[gridSize, gridSize];
+
+            Panel grid = new Panel();
+            grid.Size = new Size(gridSize * (buttonSize + 1), gridSize * (buttonSize + 1));
+            this.Size = grid.Size;
+            grid.Location = new Point(0, 0);
+            grid.BackColor = Color.Yellow;
+
+            for (byte i = 0; i < gridSize - 1; i++)
+            {
+                for (byte j = 0; j < gridSize - 1; j++)
+                {
+                    gridArr[i, j] = new Button();
+                    gridByte[i, j] = 0;
+                    gridArr[i, j].Size = new Size(buttonSize, buttonSize);
+                    gridArr[i, j].Location = new Point(buttonSize * i, buttonSize * j);
+                    gridArr[i, j].BackColor = Color.Black;
+                    gridArr[i, j].Click += Form1_Click;
+                    gridArr[i, j].Tag = new byte[2] { i, j };
+                    grid.Controls.Add(gridArr[i, j]);
+                }
+            }
+
+            this.Controls.Add(grid);
+
+            gridByte[0, gridSize / 2 - 1] = 1;
+            gridArr[0, gridSize / 2 - 1].BackColor = Color.Green;
+            prevButton = new byte[2] { 0, (byte)(gridSize / 2 - 1) };
+
+            GridSizeNum.Enabled = false;
+            InitGridButton.Enabled = false;
+            GridSizeText.Visible = false;
+            GridSizeNum.Visible = false;
+            InitGridButton.Visible = false;
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            byte gridSize = (byte)gridArr.GetLength(0);
+            byte[] pressedButton = (byte[])(((Button)(sender)).Tag);
+            if (prevButton == null)
+            {
+                prevButton = new byte[2] { pressedButton[0], pressedButton[1] };
+            }
+            else
+            {
+                if (pressedButton[0] - prevButton[0] == 0 || pressedButton[1] - prevButton[1] == 0)
+                {
+                    if (pressedButton[0] - prevButton[0] == 0)
+                    {
+                        for (byte i = Math.Min(pressedButton[1], prevButton[1]); i < 1 + Math.Max(pressedButton[1], prevButton[1]); i++)
+                        {
+                            gridArr[prevButton[0], i].BackColor = Color.Red;
+                            gridByte[prevButton[0], i] = 2;
+                        }
+                    }
+                    else
+                    {
+                        for (byte i = Math.Min(pressedButton[0], prevButton[0]); i < 1 + Math.Max(pressedButton[0], prevButton[0]); i++)
+                        {
+                            gridArr[i, prevButton[1]].BackColor = Color.Red;
+                            gridByte[i, prevButton[1]] = 2;
+                        }
+                    }
+                    prevButton = null;
+                }
+                else
+                {
+                    if (prevButton != new byte[2] { (byte)(gridSize / 2 - 1), 0 })
+                    {
+                        prevButton = null;
+                    }
+                }
+            }
+        }
+    }
+}
